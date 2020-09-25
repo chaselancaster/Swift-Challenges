36163727f3a4f61de5f8acf18d9ba1922f86895a
Chase Lancaster <chaseblancaster@gmail.com>
2020-09-25 16:54:53 -0700

    Change challenge amount

--- challenges.swift
+++ challenges.swift
@@ -1,4 +1,4 @@
-/* Function 1 - hello world */ 
+/* Function 1 - hello world - 9/25/20 */ 
 /* https://www.codewars.com/kata/523b4ff7adca849afe000035/train/swift */
 
 // Write a function `greet` that returns "hello world!"
