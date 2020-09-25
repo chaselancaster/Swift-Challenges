5387e53fa1289297f9b568b9d9b75f0a8196a1ec
Chase Lancaster <chaseblancaster@gmail.com>
2020-09-25 16:43:32 -0700

    Function 1 CodeWars challenge added

---
+++ challenges.swift
@@ -0,0 +1,8 @@
+/* Function 1 - hello world */ 
+/* https://www.codewars.com/kata/523b4ff7adca849afe000035/train/swift */
+
+// Write a function `greet` that returns "hello world!"
+
+func greet() -> String {
+  return "hello world!"
+}
